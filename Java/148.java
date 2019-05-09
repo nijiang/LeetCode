e6bf2bcb512143b5532af8387ee37f181f25c889
Ni Jiang <3512810@qq.com>
2019-05-09 20:04:10 +0800

    148 排序链表 解法：归并排序

--- Java/148.java
+++ Java/148.java
@@ -1,5 +1,50 @@
 __________________________________________________________________________________________________
+3ms
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return head == null ? null : mergeSort(head);
+    }
 
+    private ListNode mergeSort(ListNode head){
+        if(head.next == null){
+            return head;
+        }
+
+        ListNode f = head, s = head, pre = null;
+        while(f != null && f.next != null){
+            pre = s;
+            s = s.next;
+            f = f.next.next;
+        }
+        pre.next = null;
+        ListNode l = mergeSort(head);
+        ListNode r = mergeSort(s);
+        return merge(l, r);
+    }
+
+    private ListNode merge(ListNode l, ListNode r){
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while(l != null && r != null){
+            if(l.val <= r.val){
+                cur.next = l;
+                l = l.next;
+                cur = cur.next;
+            } else {
+                cur.next = r;
+                r = r.next;
+                cur = cur.next;
+            }
+        }
+        if(l != null){
+            cur.next = l;
+        }
+        if(r != null){
+            cur.next = r;
+        }
+        return dummyHead.next;
+    }
+}
 __________________________________________________________________________________________________
 
 __________________________________________________________________________________________________
